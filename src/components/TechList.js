277146e98a80c9c425f85151d8ea9b9abc8c87d9
Flavio <fbcaruso@hotmail.com>
2019-10-21 14:35:03 -0300

    Modulo04 Finalizado

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
 
+import TechItem from './TechItem';
+
 class TechList extends Component {
-  state = {
+    state = {
     newTech: '',
-    techs:[
-      'Node.js',
-      'ReactJS',
-      'React Native',
-    ]
+    techs:[]
   };
+
+  //Executado assim que o componente aparece na tela
+  componentDidMount(){
+    const techs = localStorage.getItem('techs');
+
+    if(techs){
+      this.setState({
+        techs: JSON.parse(techs)
+      })
+    }
+  }
+
+  //Executado sempre que houver alteraçoes nas props ou estado
+  componentDidUpdate(_, prevState){
+    if(prevState.techs !== this.state.techs) {
+      localStorage.setItem('techs', JSON.stringify(this.state.techs))
+    }
+  }
+
 
   handleInputChange = e => {
     this.setState({ newTech: e.target.value });
@@ -20,14 +37,24 @@ class TechList extends Component {
     this.setState({ 
       techs: [...this.state.techs, this.state.newTech], 
       newTech: ''
-    })
+    });
+  }
+
+  handleDelete = (tech) => {
+    this.setState({ techs: this.state.techs.filter(t => t !== tech) })
   }
 
   render() {
     return(
       <form onSubmit={this.handleSubmit}>
         <ul>
-          {this.state.techs.map(tech => <li key={tech}>{tech}</li>)}
+          {this.state.techs.map(tech => (
+            <TechItem 
+              key={tech} 
+              tech={tech} 
+              onDelete={() => this.handleDelete(tech)} 
+            />
+          ))}
         </ul>
         <input 
           type="text" 
